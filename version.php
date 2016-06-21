3039c7a2a9d7f2ace05a393a8bbbed422a98de16
Caio Doneda <caiodoneda@gmail.com>
2016-06-21 13:46:14 -0300

    removendo dependência com plugin wsufsc para efeito de testes

--- version.php
+++ version.php
@@ -4,6 +4,3 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_inscricoes';
 $plugin->version   = 2015040700;
 $plugin->requires  = 2013111800; //Moodle 2.6.1
-$plugin->dependencies = array(
-    'local_wsufsc' => 2014081401,
-);
